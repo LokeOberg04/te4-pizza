a2311cc8b975ba2b2f3481b30eb2ec43177cbaf4
= <=>
2023-09-18 15:26:13 +0200

    pull test

--- javascript.js
+++ javascript.js
@@ -3,12 +3,15 @@ import menu from './menu.js'
 console.log("js loaded...")
 
 console.log(menu)
-
+let pizzaList = document.createElement("ul")
 function showMenu(id) {
 
     console.log(menu[id].type)
     menu[id].pizzor.forEach((pizza) => {
 
+        let pizzaItem = document.createElement("li")
+        let pizzatext = document.createElement("p")
+        pizzatext.innerHTML = "\nPizza Name: " + pizza.name + "\nPizza Ingredients: " + pizza.ingredients + "\nPizza Price: " + pizza.price
         console.log("Pizza Name: " + pizza.name)
 
         console.log("Pizza Ingredients: " + pizza.ingredients)
