69fba40feef78aee03b431cd4feea71a598e0772
= <=>
2023-09-19 08:50:32 +0200

    class

--- menu.js
+++ menu.js
@@ -40,5 +40,31 @@ export default [
                 "price": "110 kr"
             }
         ]
+    },
+    {
+        "id": 3,
+        "type": "Specialla Pizzor",
+        "pizzor": [
+            {
+                "name": "Agels special",
+                "ingredients": "Tomatsås, ost, nutella, ballong, svamp, lök, tonfisk, vitlök, musslor, pommes, nachochips, mozarella, jalapeno, salami, truffle olja, avocado, körv, bacon, biff, kyckling, fetaost, pesto sås, bbq pulled pork, kimchi, egg, crab sticks, stekt sparris",
+                "price": "2000 kr"
+            },
+            {
+                "name": "Ejdens special",
+                "ingredients": "Tomat, ost, banan, gurka, aubergine, choklad, bönor, ajvarsås, chili",
+                "price": "150 kr"
+            },
+            {
+                "name": "Sarahs special",
+                "ingredients": 'Tomat, ost, oxfile, "kebabsås"',
+                "price": "110 kr"
+            },
+            {
+                "name": "Tims special",
+                "ingredients": 'ost, bottom ris, maxa dressing',
+                "price": "130 kr"
+            }
+        ]
     }
 ]
