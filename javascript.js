409aa70421e83fc2a024d1bb93e4a479e9300e8e
= <=>
2023-09-19 08:40:05 +0200

    menu items

--- javascript.js
+++ javascript.js
@@ -10,8 +10,19 @@ function showMenu(id) {
     menu[id].pizzor.forEach((pizza) => {
 
         let pizzaItem = document.createElement("li")
+        let pizzaName = document.createElement("h5")
         let pizzaText = document.createElement("p")
-        pizzaText.innerHTML = pizza.name + "<br><hr>" + pizza.ingredients + "<br>" + pizza.price
+        let pizzaPrice = document.createElement("p")
+
+        pizzaName.innerHTML = pizza.name + "<br><hr>"
+        pizzaText.innerHTML = pizza.ingredients
+        pizzaPrice.innerHTML = pizza.price
+
+        pizzaItem.classList.add("menuItem")
+        pizzaName.classList.add("menuItemName")
+        pizzaText.classList.add("menuItemText")
+        pizzaPrice.classList.add("menuItemPrice")
+
         console.log("Pizza Name: " + pizza.name)
 
         console.log("Pizza Ingredients: " + pizza.ingredients)
@@ -19,7 +30,9 @@ function showMenu(id) {
         console.log("Pizza Price: " + pizza.price)
 
         console.log("...")
+        pizzaItem.appendChild(pizzaName)
         pizzaItem.appendChild(pizzaText)
+        pizzaItem.appendChild(pizzaPrice)
         pizzaList.appendChild(pizzaItem)
     })
 
